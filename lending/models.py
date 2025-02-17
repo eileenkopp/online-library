91f7bb0d9eba7ca5806a463468c56ef77f3d6d1b
Shahryar Kiani <shahryarkiani04@gmail.com>
2025-02-17 18:35:45 -0500

    Add models

--- lending/models.py
+++ lending/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    book_title = models.CharField(max_length=200)
+    book_author = models.CharField(max_length=200)
+    book_genre = models.CharField(max_length=200)
+    pub_year = models.DateTimeField("date published")
+    # genre
+
+
+    def __str__(self):
+        return self.book_title + " " + self.book_author + " " + self.book_genre
+    
+class User(models.Model):
+    email = models.EmailField(max_length=254)
