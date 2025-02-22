18f2895bb5c2012d0eb00796988b146ccbdb419a
Nick Moir <bfkiwikid@icloud.com>
2025-02-22 15:00:22 -0500

    tester

--- lending/views.py
+++ lending/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
+
 
 # Create your views here.
 class IndexView(TemplateView):
@@ -8,3 +10,12 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+def login(request):
+    return render(request, 'lending/login.html')
+
+
+def logout_view(request):
+    from django.contrib.auth import logout
+    logout(request)
+    return redirect('https://accounts.google.com/logout?continue=http://127.0.0.1:8000/lending/login/')
