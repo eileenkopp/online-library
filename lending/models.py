86dfee8eb952f817ce77aba945eb14cb1467d63d
Patricio Anduaga <patoanduaga@gmail.com>
2025-02-22 02:44:23 -0500

    Created 3 separate columns

--- lending/models.py
+++ lending/models.py
@@ -1,22 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Genre(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.name
-
 class Book(models.Model):
-    book_title = models.CharField(max_length=200)
-    book_author = models.CharField(max_length=200)
-    genres = models.ManyToManyField(Genre, related_name="books")
-    #Alternate: genres = ArrayField(models.CharField(max_length=50), blank=True, default=list)
+    book_title = models.CharField(max_length=50)
+    book_author = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
     pub_year = models.PositiveIntegerField("Year Published")
+    summary = models.CharField(max_length=250)
     in_stock = models.BooleanField(default=False) 
+    #book_cover = models.ImageField(upload_to='book_covers/')
 
     def __str__(self):
-        return self.book_title + " " + self.book_author + ", " + str(self.pub_year)
+        return self.book_title + ". By " + self.book_author + " (" + str(self.pub_year) + ")"
     
 class User(models.Model):
     email = models.EmailField(max_length=254)
