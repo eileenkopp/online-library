0897ad2d7cc3ab68e9a26fe3a0e910b2b32f06ee
Patricio Anduaga <patoanduaga@gmail.com>
2025-02-22 04:45:30 -0500

    Added book cover to model/display

--- lending/models.py
+++ lending/models.py
@@ -8,7 +8,7 @@ class Book(models.Model):
     pub_year = models.PositiveIntegerField("Year Published")
     summary = models.CharField(max_length=250)
     in_stock = models.BooleanField(default=False) 
-    #book_cover = models.ImageField(upload_to='book_covers/')
+    book_cover = models.ImageField(upload_to='media/book_covers')
 
     def __str__(self):
         return self.book_title + ". By " + self.book_author + " (" + str(self.pub_year) + ")"
