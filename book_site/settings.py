942cd7dde2467cc03bfc569f349678a304c0b411
Shahryar Kiani <shahryarkiani04@gmail.com>
2025-02-22 22:54:28 -0500

    Fix settings.py

--- book_site/settings.py
+++ book_site/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lending.apps.LendingConfig',
     'bootstrap5',
-    'social_django'
+    'social_django',
+    'lending'
 ]
 
 MIDDLEWARE = [
