9d2fc7d95519bd90512853606f387183963486e7
Shahryar Kiani <shahryarkiani04@gmail.com>
2025-02-22 22:07:51 -0500

    Implement index list view

--- lending/views.py
+++ lending/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
+
 
 from .forms import BookForm
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Book
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "lending/index.html"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+    context_object_name = "book_list"
+    def get_queryset(self):
+        return Book.objects.all()
 
 def login(request):
     return render(request, 'lending/login.html')
@@ -25,6 +26,8 @@ def logout_view(request):
 def add_book(request):
     if request.method == 'POST':
         form = BookForm(request.POST)
+        if not request.user or not request.user.is_staff:
+            return HttpResponseForbidden('Permission Denied')
         if form.is_valid():
             form.save()
             return redirect('lending:index')
