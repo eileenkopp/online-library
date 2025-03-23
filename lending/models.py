96647131306c6fe3a46235fb6a300bf2e81d9ea3
Shahryar Kiani <shahryarkiani04@gmail.com>
2025-03-23 18:49:56 -0400

    Create request and collection models, update book model

--- lending/models.py
+++ lending/models.py
@@ -10,7 +10,8 @@ class Book(models.Model):
     summary = models.TextField(max_length=500, default="")
     in_stock = models.BooleanField(default=False) 
     book_cover = models.ImageField(upload_to='media/book_covers')
-
+    total_copies = models.PositiveIntegerField("Total Copies")
+    total_available = models.PositiveIntegerField("Total Available")
     def __str__(self):
         return self.book_title + ". By " + self.book_author + " (" + str(self.pub_year) + ")"
 
@@ -21,3 +22,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+        
+class Collection(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    books = models.ManyToManyField(Book)
+    private = models.BooleanField() # Only librarians can make private collections
+    collection_name = models.CharField(max_length=100)
+
+class Request(models.Model):
+    requester = models.OneToOneField(User, on_delete=models.CASCADE)
+    requested_book = models.OneToOneField(Book, on_delete=models.CASCADE)
+
