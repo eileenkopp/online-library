a8897901c5375749e7feffa750ab348ceff8c824
eileenkopp <jjk2jy@vrginia.edu>
2025-03-28 15:37:59 -0400

    updated forms.py to add the new fields we added to book model

--- lending/forms.py
+++ lending/forms.py
@@ -5,7 +5,17 @@ from .models import Profile
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['book_title', 'book_author', 'book_genre', 'pub_year', 'summary', 'in_stock', 'book_cover']
+        fields = [
+            'book_title',
+            'book_author',
+            'book_genre',
+            'pub_year',
+            'summary',
+            'book_cover',
+            'total_copies',
+            'total_available',
+            'in_stock'
+        ]
         widgets = {
             'publication_year': forms.NumberInput(attrs={'min': 1000, 'max': 9999}),
         }
