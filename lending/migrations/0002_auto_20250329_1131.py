e369f5ba5411b952f489014100ab7b07f5277280
Shahryar Kiani <shahryarkiani04@gmail.com>
2025-04-13 02:00:34 -0400

    Fix migrations

--- lending/migrations/0002_auto_20250329_1131.py
+++ lending/migrations/0002_auto_20250329_1131.py
@@ -6,10 +6,3 @@ class Migration(migrations.Migration):
         ('lending', '0001_initial'),  # or whatever your latest is
     ]
 
-    operations = [
-        migrations.AddField(
-            model_name='book',
-            name='total_copies',
-            field=models.PositiveIntegerField(default=1, verbose_name='Total Copies'),
-        ),
-    ]
