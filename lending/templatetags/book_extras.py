fa79cfdbdab4e140e0d6161c7054eec3dab279e6
eileenkopp <jjk2jy@vrginia.edu>
2025-04-29 11:28:31 -0400

    changed location tag in book detail

--- lending/templatetags/book_extras.py
+++ lending/templatetags/book_extras.py
@@ -4,4 +4,14 @@ register = template.Library()
 
 @register.filter
 def get_range(value):
-    return range(value) 
+    return range(value)
+
+@register.filter
+def format_location(location_code):
+    location_map = {
+        'SHANNON': 'Shannon Library',
+        'CLEMONS': 'Clemons Library',
+        'BROWN': 'Brown Science and Engineering Library',
+        'ON_LOAN': 'On Loan'
+    }
+    return location_map.get(location_code, location_code)
